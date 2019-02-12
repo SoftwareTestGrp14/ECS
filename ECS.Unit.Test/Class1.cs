bdcc4c1ff4b3773ba29a0f6f9a22f3cd9a0d4370
Oskarthansen <oskarthansen@outlook.dk>
2019-02-12 13:49:10 +0100

    Made class "FakeHeater"

--- ECS.Unit.Test/Class1.cs
+++ ECS.Unit.Test/Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ECS.Unit.Test
 {
-    public class Class1
+    public class ECSUnitTest
     {
     }
 }
---
+++ ECS.Unit.Test/FakeHeater.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECS.Refac;
+
+namespace ECS.Unit.Test
+{
+    public class FakeHeater : IHeater
+    {
+        public int TurnOnNo { get; private set; }
+        public int TurnOffNo { get; private set; }
+        public int RunTestNo { get; private set; }
+
+        public void TurnOn()
+        {
+            ++TurnOnNo;
+        }
+
+        public void TurnOff()
+        {
+            ++TurnOffNo;
+        }
+
+        public bool RunSelfTest()
+        {
+            ++RunTestNo;
+            return true;
+        }    
+    }
+
+
+
+
+}
