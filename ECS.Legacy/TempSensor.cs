178918b8e658dcc7c3a0897b4837495e5c7f6acc
Jonas <au544262@post.au.dk>
2019-02-12 12:44:14 +0100

    Add project files.

---
+++ ECS.Legacy/TempSensor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ECS.Legacy
+{
+
+    public interface ITempSensor
+    {
+        int GetTemp();
+        bool RunSelfTest();
+
+    }
+
+    internal class TempSensor:ITempSensor
+    {
+        private Random gen = new Random();
+
+        public int GetTemp()
+        {
+            return gen.Next(-5, 45);
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
